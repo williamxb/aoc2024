cf076b1c9a9fac376d80988b4005b5336dc5f8b5
Will Knight <45063456+williamxb@users.noreply.github.com>
2024-12-09 01:54:51 +0000

    Day 01 🎄

--- README.md
+++ README.md
@@ -11,7 +11,7 @@
 
 <!--SOLUTIONS-->
 
-![Day](https://badgen.net/badge/01/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 ![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
@@ -69,9 +69,9 @@ npm start 1
 
 ```
 Day 01
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 0.769ms
+Time part 2: 10.629ms
+Both parts: 11.399ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 0/50
-Total time: -
+Total stars: 2/50
+Total time: 11.399ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day01/index.js
@@ -0,0 +1,68 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput) => rawInput;
+
+const part1 = (rawInput) => {
+  const input = parseInput(rawInput);
+
+  // assign variables: sum: Integer, b: Array 
+  // parse input into array with regex by removing whitespace
+  let sum = 0, b = [], a = input.split(/\s+/);
+
+  // break down single array into the two columns
+  for (let i = 0; i < a.length; i++)
+    (i % 2 ? b : a)[Math.floor(i / 2)] = a[i];
+  
+  // remove now empty items from first array
+  a.splice(a.length / 2);
+  
+  // sort ascending
+  let aSort = a.sort()
+  let bSort = b.sort()
+
+  // Math.abs to get non-negative difference
+  for (let i = 0; i < a.length; i++) {
+    sum+=Math.abs(aSort[i]-bSort[i])
+  }
+  return sum;
+};
+
+const part2 = (rawInput) => {
+  // parse input & arrays round 2
+  const input = parseInput(rawInput);
+  let sum = 0, b = [], a = input.split(/\s+/);
+
+  for(let i = 0; i< a.length; i++)
+    (i % 2 ? b : a)[Math.floor(i / 2)] = a[i];
+  
+  a.splice(a.length / 2);
+
+  // use Array.filter to get number of occurences 
+  for (let i = 0; i < a.length; i++) {
+    sum += a[i] * b.filter((x) => x == a[i]).length
+  }
+  return sum;
+};
+
+run({
+  part1: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
